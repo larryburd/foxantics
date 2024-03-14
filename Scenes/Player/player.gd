05bf1b1bb7251f32ca1b69a500ecbdab3b2bb706
laurence burden <laurenceburden@gmail.com>
2024-03-13 20:20:26 -0500

    Added physics to the tiles and gravity to player

---
+++ Scenes/Player/player.gd
@@ -0,0 +1,18 @@
+extends CharacterBody2D
+
+class_name Player
+
+@onready var sprite_2d = $Sprite2D
+@onready var animation_player = $AnimationPlayer
+
+const GRAVITY: float = 1000.0
+
+func _ready():
+	pass
+	
+
+func _physics_process(delta):
+	if is_on_floor() == false:
+		velocity.y += GRAVITY * delta
+	
+	move_and_slide()
